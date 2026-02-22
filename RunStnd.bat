a59d3736fedd146823b3908128496c2cf1b5f9d3
patelboss <83952959+patelboss@users.noreply.github.com>
2026-02-22 12:04:06 +0530

    Update RunStnd.bat

--- RunStnd.bat
+++ RunStnd.bat
@@ -26,6 +26,7 @@ IF %ERRORLEVEL% EQU 0 (
 set "CUR_DIR=%~dp0"
 
 set "GPEDIT_FILE=%CUR_DIR%gpedit-enabler.bat"
+set "EableLan=%CUR_DIR%EableLan.reg"
 set "SSH_FILE=%CUR_DIR%enablessh.ps1"
 set "PS_FILE=%CUR_DIR%Noinstallation.ps1"
 
@@ -43,7 +44,7 @@ echo.
 
 if exist "%GPEDIT_FILE%" (
 
-    echo [1/3] Running gpedit enabler...
+    echo [1/4] Running gpedit enabler...
     call "%GPEDIT_FILE%"
 
     echo [OK] gpedit enabler finished.
@@ -57,13 +58,27 @@ if exist "%GPEDIT_FILE%" (
 )
 
 
+if exist "%GPEDIT_FILE%" (
+
+    echo [2/4] Running Lan enabler...
+    call "%EableLan%"
+
+    echo [OK] lan enabler finished.
+    echo.
+
+) else (
+
+    echo [WARN] EableLan.reg not found!
+    echo Skipping...
+    echo.
+)
 :: ----------------------------------------
 :: Run SSH Enabler
 :: ----------------------------------------
 
 if exist "%SSH_FILE%" (
 
-    echo [2/3] Enabling SSH...
+    echo [3/4] Enabling SSH...
 
     powershell -NoProfile -ExecutionPolicy Bypass -File "%SSH_FILE%"
 
@@ -84,7 +99,7 @@ if exist "%SSH_FILE%" (
 
 if exist "%PS_FILE%" (
 
-    echo [3/3] Running main setup script...
+    echo [4/4] Running main setup script...
 
     powershell -NoProfile -ExecutionPolicy Bypass -File "%PS_FILE%"
 
